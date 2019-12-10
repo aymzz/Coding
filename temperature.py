ba8e57cc591772e04e6125ff17b2d4846f7ded31
aymzz <56570557+aymzz@users.noreply.github.com>
2019-12-09 18:55:57 -0800

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,13 @@
+print("What is the temperature?")
+temperature = int(input())
+print("The temperature is", temperature)
+
+
+if temperature == 32:
+  print("It is exactly freezing temperature")
+
+if temperature >32: 
+  print("it is above freezing temperature")
+
+if temperature <32:
+  print("it is below freezzing temperature")
