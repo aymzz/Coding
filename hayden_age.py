60c2b749f86671a4ffcac9ddc2f4e803ab4af3f6
aymzz <56570557+aymzz@users.noreply.github.com>
2019-12-09 18:51:21 -0800

    adding age

---
+++ hayden_age.py
@@ -0,0 +1,11 @@
+print("what is your age?")
+age = input()
+age = int(age)
+if age < 12: # condition is age < 12
+  print("You are younger than Hayden")
+  print("Your age is ", age)
+if age > 12:
+  print("You are older than Hayden")
+  print("Hayden's age is ", 12)
+if age == 12:
+  print("You are Hayden's age")
---
+++ number_add.py
@@ -0,0 +1,13 @@
+print("What is number #1?")
+number_one = int(input()) # 3
+print("What is number #2?")
+number_two = int(input()) # 5
+print(number_one, "adding", number_two, "is equal to")
+print(number_one + number_two)
+# print("What is your name?")
+# name = input()
+# print("Hello, ", name)
+
+
+
+
